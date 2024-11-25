6685dd9d7742cd5046c5ccf9f10c4cc98a368b55
Satomo <satrioutomo344@gmail.com>
2024-11-25 17:15:45 +0700

    Add files via upload

---
+++ loop_biodata.py
@@ -0,0 +1,12 @@
+biodata={
+
+    "Nama\t\t" : "Satrio Putro Utomo",
+    "Kelas\t\t" : "Rombel 2",
+    "Tanggal Lahir\t" : "15 - O1 - 2005",
+    "No Hp\t\t" : "085633767329",
+    "Alamat\t\t" : "Perumahan Pondok Asri 2 Payaman",
+    "Hobi\t\t" : [ "Hobi 1: Bersepeda","Hobi 2: Kulineran","Hobi 3: Memasak"],
+    }
+
+for i,j in biodata.items():
+    print(i,j)
